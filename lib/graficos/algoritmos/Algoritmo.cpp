fee29d8344bdc3284ed9d2074a37e9f396a62261
aechavarris <aechavarris4@gmail.com>
2021-11-30 16:50:05 +0100

    Cosas

---
+++ lib/graficos/algoritmos/Algoritmo.cpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Algoritmo.h"
+
+Algoritmo::Algoritmo(){};
+
---
+++ lib/graficos/algoritmos/Algoritmo.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "../../lib/graficos/Grafo.h"
+
+
+
+using namespace std;
+
+#ifndef _Algoritmo
+#define _Algoritmo
+
+class Algoritmo {
+    public:
+        
+        /*
+         * Constructor de la clase Algoritmo
+         */
+        Algoritmo();
+
+
+        // ---------------------------------------
+        // ------------- OPERACIONES -------------
+        // ---------------------------------------
+
+        /*
+         * Pre:
+         * Post:
+         */
+        
+};
+
+#include  "Algoritmo.cpp"
+#endif
